7ae82447236b7cd3f5d56f316430b6dfdd3eaa6d
DanielSantamariaWB <63067971+DanielSantamariaWB@users.noreply.github.com>
2022-06-13 13:12:40 -0500

    feat(artillery-package):create initial performance test flow for facade create user onboarding (#3)
    * feat(test): add get user by id performance test
    
    * Remove node_modules
    
    * feat(test): add get interest level zero perfromance test set up
    
    * feat(artillery-package): create initial performance test flow for create user onboardign
    
    * fix(artillery-package): create random interest selector before eache request
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: Jesus Roncal <97462783+jesusroncal-wordbox@users.noreply.github.com>
    Co-authored-by: Jesus Roncal <jesus@wordbox.ai>

---
+++ artillery/functions.js
@@ -0,0 +1,35 @@
+const faker = require( 'faker' );
+const moment = require('moment');
+let levelzerointerests = require('./interests.json');
+
+let unlockedInterests = [];
+for(let i=0 ; i < 6 ; i++){
+    var r = Math.floor(Math.random() * levelzerointerests.length);
+    unlockedInterests.push(levelzerointerests[r]);
+    levelzerointerests.splice(r, 1);
+}
+
+exports.createRandomUser = (req, context, events, next) => {
+    var levelpercentage = (Math.floor(Math.random() * ( 9 - 1 + 1)) + 1)*10
+    var currentdate = moment().format("DD/MM/YYYY")
+    var userpayload = {
+        "id":"",
+        "name":"",
+        "email":"",
+        "urlphoto":"",
+        "registrationdate":currentdate,
+        "levelpercentage": levelpercentage
+    };
+    userpayload.id = "performancetestuser"+faker.datatype.uuid();
+    userpayload.name = faker.name.findName();
+    userpayload.email = faker.internet.email();
+    userpayload.urlphoto = faker.image.avatar();
+    context.vars.userpayload = userpayload;
+    return next();
+}
+
+exports.selectRandomInterests = (req, context, events, next) => {
+    context.vars.unlockedInterests = unlockedInterests;
+    context.vars.boxesInterests = levelzerointerests;
+    return next()
+}
