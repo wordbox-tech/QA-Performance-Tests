43207141ef36072e527460dfc1c48a61323b2e47
Jesus Roncal <97462783+jesusroncal-wordbox@users.noreply.github.com>
2022-06-30 01:22:57 -0500

    feat(get_users_data_and_export_to_csv-script-module): get, filter and export users by prefix in id (#11)
    * Got, filtered and exported users by prefix in id in get_users_data_and_export_to_csv python script module
    * Updated users.csv file in artillery package

--- python-scripts/get_users_data_and_export_to_csv.py
+++ python-scripts/get_users_data_and_export_to_csv.py
@@ -2,6 +2,7 @@ from typing import Final
 
 from authorization_handler import firebase_auth_module
 from common_constants import USER_ID_ALIAS
+from common_constants import USER_ID_PREFIX
 from common_constants import USER_RANK_ALIAS
 from common_constants import USERS_COLLECTION
 from csv_utils import export_to_csv
@@ -12,6 +13,7 @@ from providers import get_sync_firestore_auth_module
 
 
 CSV_FILE_NAME: Final[str] = "./artillery/users.csv"
+EMPTY_STRING: Final[str] = ""
 
 load_dotenv()
 firebase_auth_module()
@@ -25,7 +27,12 @@ def get_users_data_and_export_to_csv(
     field_names = [USER_ID_ALIAS, USER_RANK_ALIAS]
     users_collection = firestore_client.collection(USERS_COLLECTION)
     users_data = get_collection(coll_ref=users_collection, field_names=field_names)
-    export_to_csv(file_name, field_names, users_data)
+    users_data_filter_by_prefix_in_id = [
+        user
+        for user in users_data
+        if user.get(USER_ID_ALIAS, EMPTY_STRING).startswith(USER_ID_PREFIX)
+    ]
+    export_to_csv(file_name, field_names, users_data_filter_by_prefix_in_id)
 
 
 if __name__ == "__main__":
